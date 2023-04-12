00d2209b2182a4a73d023d6c84a91ed0038c7b2e
Geethanjali-Allam <ga1107@unh.edu>
2023-04-12 10:46:51 -0400

    created a new file for books.py

---
+++ book.py
@@ -0,0 +1,21 @@
+# Book class
+
+class Book:
+    """Represents a book with a title, author, and genre."""
+
+    def init(self, title, author, genre, borrower=None):
+        """
+        Initializes a new instance of the Book class with the specified title, author, genre, and optional borrower.
+        Parameters:
+        title (str): The title of the book.
+        author (str): The author of the book.
+        genre (str): The genre of the book.
+        borrower (str, optional): The name of the borrower who has checked out the book.
+
+        Returns:
+        None
+        """
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.borrower = borrower
