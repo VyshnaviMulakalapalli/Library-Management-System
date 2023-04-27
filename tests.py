1ea7d4ce0fcf470a0f8784eb9880884b524e3c60
VyshnaviMulakalapalli <vm1096@usnh.edu>
2023-04-26 22:30:18 -0400

    add test for remove_book method

--- tests.py
+++ tests.py
@@ -18,3 +18,14 @@ class LibraryTests(unittest.TestCase):
 
         # Assert that the book has been added to the library's catalog
         self.assertTrue(any(book.title == "Test Book" for book in self.library.books))
+
+    def test_remove_book(self):
+        """Test remove_book method."""
+        # Add a book to the library
+        self.library.add_book("Test Book1", "Test Author1", "Test Genre1")
+
+        # Remove the book from the library's catalog
+        self.library.remove_book("Test Book1")
+
+        # Assert that the book has been removed from the library's catalog
+        self.assertFalse(any(book.title == "Test Book1" for book in self.library.books))
